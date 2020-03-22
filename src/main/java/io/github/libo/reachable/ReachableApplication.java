311f0b942300bf58b0b53705f374f04f76c1dc9d
libo <libo-0@foxmail.com>
2020-03-22 17:41:10 +0800

    init

---
+++ src/main/java/io/github/libo/reachable/ReachableApplication.java
@@ -0,0 +1,13 @@
+package io.github.libo.reachable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReachableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/libo/reachable/ReachableApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.libo.reachable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReachableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
